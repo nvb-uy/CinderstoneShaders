433a3bd0ea6055be814994d9d92649e9810351c5
nvb-uy <elocindev@lunapixel.gg>
2025-01-19 11:51:22 -0300

    feat(black hole and cosmic glow)

--- shaders/include/sky/sky.glsl
+++ shaders/include/sky/sky.glsl
@@ -240,17 +240,47 @@ vec3 draw_sky(vec3 ray_dir) {
 #include "/include/sky/atmosphere.glsl"
 
 const float sun_solid_angle = cone_angle_to_solid_angle(sun_angular_radius);
-const vec3 end_sun_color = vec3(1.0, 0.5, 0.25);
+const vec3 end_sun_color = vec3(0.80, 0.1, 0.4);
+
+float rand(vec2 co){
+    return fract(sin(dot(co.xy ,vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+vec3 getCosmicGlow(vec3 ray_dir, int iterations) {
+    vec3 totalGlow = vec3(0.0);
+
+    for (int i = 0; i < iterations; i++) {
+        vec3 randomPosition = vec3(rand(vec2(i, i)), rand(vec2(i + 1, i + 1)), rand(vec2(i + 2, i + 2)));
+        vec3 cosmicColor = vec3(rand(vec2(i + 3, i + 3)), rand(vec2(i + 4, i + 4)), rand(vec2(i + 5, i + 5))) * 0.15;
+
+        float glowRadius = 1.5;
+        float distanceToGlow = length(ray_dir - randomPosition);
+        float glow = smoothstep(glowRadius, 0.0, distanceToGlow);
+        totalGlow += cosmicColor * glow / 8 * END_COSMIC_GLOW_INTENSITY;
+    }
+
+    return totalGlow;
+}
 
 vec3 draw_sun(vec3 ray_dir) {
 	float nu = dot(ray_dir, sun_dir);
 	float r = fast_acos(nu);
 
+	ray_dir.z *= -0.1; ray_dir.x *= -1.52;
+
+	ray_dir *= mat3(
+        cos(radians(-90.0)), -sin(radians(90.0)), 1.0,
+        sin(radians(90.0)), cos(radians(-90.0)), 2.0,
+        -0.0, 0.0, 0.0
+    );
+
+	ray_dir += 0.15;
+
 	// Sun disk
 
 	const vec3 alpha = vec3(0.6, 0.5, 0.4);
 	float center_to_edge = max0(sun_angular_radius - r);
-	vec3 limb_darkening = pow(vec3(1.0 - sqr(1.0 - center_to_edge)), 0.5 * alpha);
+	vec3 limb_darkening = pow(vec3(1.0 - sqr(1.0 - center_to_edge)), -0.225 * alpha);
 	vec3 sun_disk = vec3(r < sun_angular_radius);
 
 	// Solar flare effect
@@ -258,18 +288,37 @@ vec3 draw_sun(vec3 ray_dir) {
 	// Transform the coordinate space such that z is parallel to sun_dir
 	vec3 tangent = sun_dir.y == 1.0 ? vec3(1.0, 0.0, 0.0) : normalize(cross(vec3(0.0, 1.0, 0.0), sun_dir));
 	vec3 bitangent = normalize(cross(tangent, sun_dir));
-	mat3 rot = mat3(tangent, bitangent, sun_dir);
+	mat3 rot = mat3(tangent, bitangent, sun_dir) ;//* -18.37;
 
 	// Vector from ray dir to sun dir
-	vec2 q = ((ray_dir - sun_dir) * rot).xy;
-
+	vec2 q = ((ray_dir + sun_dir) * rot).xy;
+	
 	float theta = fract(linear_step(-pi, pi, atan(q.y, q.x)) + 0.015 * frameTimeCounter - 0.33 * r);
 
-	float flare = texture(noisetex, vec2(theta, r - 0.025 * frameTimeCounter)).x;
-	      flare = pow5(flare) * exp(-25.0 * (r - sun_angular_radius));
-		  flare = r < sun_angular_radius ? 0.0 : flare;
+	float flare1 = texture(noisetex, vec2(theta, r - 0.025 * frameTimeCounter * 1.25)).x;
+    flare1 = pow5(flare1) * exp(-25.0 * (r - sun_angular_radius));
+    flare1 = r < sun_angular_radius ? 0.0 : flare1;
 
-	return end_sun_color * rcp(sun_solid_angle) * max0(sun_disk + 0.1 * flare);
+    float theta2 = fract(linear_step(-pi, pi, atan(q.y, q.x)) + -0.0002 * frameTimeCounter - 0.33 * (r + 0.05));
+    float flare2 = texture(noisetex, vec2(theta2, r - 0.025 * frameTimeCounter)).x;
+    flare2 = pow5(flare2) * exp(-20 * (r - sun_angular_radius / 2));
+    flare2 = r < sun_angular_radius ? 0.0 : flare2;
+
+    vec3 flare_color1 = end_sun_color;
+    vec3 flare_color2 = vec3(0.8, 0.3, 0.0);
+
+	// Black hole time!
+    return
+		#ifdef END_COSMIC_GLOW
+		getCosmicGlow(ray_dir, 5) +
+		#endif
+		#ifdef END_BLACK_HOLE
+		((vec3(0.0, 0.0, 0.0) * max0(sun_disk) ) * 100 
+		+ flare_color1 * rcp(sun_solid_angle) * max0(0.5 * flare1) 
+		+ flare_color2 * rcp(sun_solid_angle) * max0(1.5 * flare2));
+		#else
+		vec3(0.0);
+		#endif
 }
 
 vec3 draw_sky(vec3 ray_dir) {
